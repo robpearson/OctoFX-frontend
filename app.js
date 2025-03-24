26ffa5e035b92f0da4910dbb1fb9912cc7cb2cd1
Rob Pearson <rob.pearson@me.com>
2025-03-24 12:59:14 +1000

    AUTO-4 Introducing bug.

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Check Price in your city. Check Price in your city.')
 })
 
 app.listen(port, () => {
