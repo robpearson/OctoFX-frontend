a8168ccc33a2f093b32a54a2080ea7b0474622dc
Rob Pearson <rob.pearson@me.com>
2025-06-02 23:22:50 +0800

    Integrated Datadog APM. Cleaned up App.js.

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
+const tracer = require('dd-trace').init();
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-  res.send('Check Price in your city. Check Price in your city.')
+  res.send('OctoFX 2025 ... ')
+})
+
+app.get('/rates', (req, res) => {
+  res.send('USD: xx, CAD: xy, GBP: xz, SGP: yy AUD: yz')
+})
+
+app.get('/trade', (req, res) => {
+  res.send('Convert currency 1 to currency 2 ...')
+})
+
+app.get('/daily-report', (req, res) => {
+  res.send('OctoFX Daily report ... ')
 })
 
 app.listen(port, () => {
-  console.log(`Bug fix for demo purposes. Listening on port ${port}`)
+  console.log(`OctoFX 2025. Listening on port ${port}`)
 })
